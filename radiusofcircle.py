75e6760ce0e601e8faf362e512c111ed92b2f25b
Jaspreet1997 <singhjaspreet1319@gmail.com>
2021-05-22 10:13:00 -0400

    raduis of circle code added

---
+++ radiusofcircle.py
@@ -0,0 +1,7 @@
+import math
+
+radius = int(input("please enter the number to find out the radius of the circle"))
+
+area= math.pi * radius * radius
+
+print("The Area of the circle is ***** :{0}".format(area))
