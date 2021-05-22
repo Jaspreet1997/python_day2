7299cf0304ed591ca5928672b156dc24f27f32f7
Jaspreet1997 <singhjaspreet1319@gmail.com>
2021-05-22 10:00:31 -0400

    datetime file added

---
+++ dateandtime.py
@@ -0,0 +1,5 @@
+import datetime
+now = datetime.datetime.now()
+print("Here is the Current Date and Time Showing")
+print("***********************************************")
+print(now.strftime("%y-%m-%d %H:%M:%S"))
